70d03a52a3df5a7311dceb49065415eb676bb9af
sippu0407 <suryabha.singh.0407@gmail.com>
2023-11-24 22:35:19 +0530

    done with end to end implimentation of city api

--- src/index.js
+++ src/index.js
@@ -1,33 +1,22 @@
 const express=require('express');
 const bodyParser=require('body-parser');
-const {PORT}=require('./config/severConfig');
 
-// const {City}=require('./models/index');
-const cityRepo=require('./repository/city-repository');
+const {PORT}=require('./config/severConfig');
+const apiRoutes=require('./routes/index');
+
 
 const setupAndStartServer=async()=>{
 
-    //intialize express app
     const app=express();
-    //making middleware available for all routes so using it globally
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    
+    app.use('/api',apiRoutes);
 
     app.listen(PORT,async ()=>{
-       const repo=new cityRepo();
-       await  repo.createCity({name:"varanasi"});
-       await repo.deleteCityById(9);
-       const data={name:"kashi"};
-       await repo.updateCity({cityId:10,data:data});
-        // repo.updateCity({cityid:12,data:data}); this will give error b/c cityid is not what we have to pass to destructured object
-
-
-      await repo.deleteAllCities();
-
        console.log(`server is running on PORT : ${PORT}`); //writing in tempelated string.
-
-
-    })
+   })
 
 }
 
---
+++ src/routes/v1/index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const cityController=require('../../controllers/city-controller');
+
+const router=express.Router();
+
+// /api/v1/city  or /api/v1/city/:id uri handle karna hai to correct controller function
+router.post('/city',cityController.create);
+router.get('/city/:id',cityController.get);
+router.patch('/city/:id',cityController.update);
+router.delete('/city/:id',cityController.destroy);
+
+module.exports=router;
